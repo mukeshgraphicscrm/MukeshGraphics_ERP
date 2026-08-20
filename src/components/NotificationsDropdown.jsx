599c85a290be0b69fd916a4d3b873f51a22dfefa
mukeshgraphicscrm <mukeshgraphicscrm@gmail.com>
2026-08-20 15:25:44 +0530

    Mukesh Graphics ERP

--- src/components/NotificationsDropdown.jsx
+++ src/components/NotificationsDropdown.jsx
@@ -41,7 +41,7 @@ export default function NotificationsDropdown({ isOpen, onClose }) {
           )}
         </div>
         
-        <div className="max-h-96 overflow-y-auto">
+        <div className="max-h-[235px] overflow-y-auto">
           {notifications.length === 0 ? (
             <div className="p-6 text-center text-gray-500">
               <Bell className="w-8 h-8 mx-auto mb-2 text-gray-300" />

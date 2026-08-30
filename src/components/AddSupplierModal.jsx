2638a410ea0dcfa1ca4c7e08790007dc77e0ac31
mukeshgraphicscrm <mukeshgraphicscrm@gmail.com>
2026-08-30 18:55:33 +0530

    Mukesh Graphics ERP

--- src/components/AddSupplierModal.jsx
+++ src/components/AddSupplierModal.jsx
@@ -57,7 +57,7 @@ export default function AddSupplierModal({ isOpen, onClose, onSupplierAdded, sup
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
+    setFormData((prev) => ({ ...prev, [name]: value.toUpperCase() }));
   };
 
   const handleSubmit = async (e) => {
